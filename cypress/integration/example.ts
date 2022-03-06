6b264f1f6af817266907550f8c159ede7be55158
kulbi <kulbat.m@gmail.com>
2022-03-06 22:52:06 +0100

    implement getCurrentWeather, tests implemented

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -7,9 +7,56 @@ describe('#GET example', () => {
     const request = getExample();
 
     //then
-    resultIs(request, { example: true });
+    resultIs(request, { example: false });
   });
 });
+
+describe('#GET current_weather', () => {
+  it('should return valid data', () => {
+    // given
+
+    //when
+    const request = cy.request(
+      'GET',
+      `http://localhost:3001/current_weather?lat=51&lon=41`,
+    );
+
+    //then
+    request.should((response) => {
+      expect(response.body.temperature).to.be.a('number');
+      expect(response.body.pressure).to.be.a('number');
+      expect(response.body.humidity).to.be.a('number');
+    });
+  });
+
+  it('should return 400 if lon/lat not provided', () => {
+    // given
+
+    //when
+    const request = cy.request({
+      method: 'GET',
+      url: `http://localhost:3001/current_weather?lat=51`,
+      failOnStatusCode: false,
+    });
+
+    //then
+    request.should((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.message).to.eq('lon is required');
+    });
+  });
+});
+
+function givenWeatherApiRespondsWith(body: any) {
+  cy.intercept(
+    'GET',
+    'https://community-open-weather-map.p.rapidapi.com/weather',
+    {
+      statusCode: 201,
+      body: body,
+    },
+  );
+}
 
 function apiIsAvailable() {}
 
@@ -19,9 +66,11 @@ function getExample(): Cypress.Chainable<Cypress.Response<any>> {
 
 async function resultIs(
   request: Cypress.Chainable<Cypress.Response<any>>,
-  result: { example: boolean },
+  result: any,
 ) {
   request.should((response) => {
+    cy.log(response.body, 'body');
+    cy.log(result, 'result');
     expect(response.body).to.eql(result);
   });
 }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, Param, Query } from '@nestjs/common';
+import axios from 'axios';
+import { mainModule } from 'process';
 
 @Controller()
 export class AppController {
   @Get('/example')
   getHello() {
     return { example: true };
+  }
+
+  @Get('/current_weather')
+  async getCurrentWeather(
+    @Query('lat') lat: number,
+    @Query('lon') lon: number,
+  ): Promise<{
+    temperature: number;
+    pressure: number;
+    humidity: number;
+  }> {
+    if (!lat) {
+      throw new HttpException('lat is required', 400);
+    }
+    if (!lon) {
+      throw new HttpException('lon is required', 400);
+    }
+
+    const response = await axios.request<{
+      main: { temp: number; pressure: number; humidity: number };
+    }>({
+      method: 'GET',
+      url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+      params: {
+        lat: lat,
+        lon: lon,
+        units: 'metric',
+      },
+      headers: {
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        'x-rapidapi-key': process.env.RAPID_API_KEY,
+      },
+    });
+
+    const returnedObject = {
+      temperature: response.data.main.temp,
+      pressure: response.data.main.pressure,
+      humidity: response.data.main.humidity,
+    };
+
+    return returnedObject;
   }
 }
