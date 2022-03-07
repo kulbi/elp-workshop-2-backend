da13e3576b89226e5f45146aafba42209289eaad
kulbi <kulbat.m@gmail.com>
2022-03-07 14:17:18 +0100

    2 apps working, added 1 test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, HttpException, Param, Query } from '@nestjs/common';
+import { Controller, Get, HttpException, Query } from '@nestjs/common';
 import axios from 'axios';
-import { mainModule } from 'process';
 
 @Controller()
 export class AppController {
@@ -13,11 +12,12 @@ export class AppController {
   async getCurrentWeather(
     @Query('lat') lat: number,
     @Query('lon') lon: number,
+    @Query('alternateSource') alternateSource: string,
   ): Promise<{
     temperature: number;
     pressure: number;
     humidity: number;
-    country: string;
+    source: string;
   }> {
     if (!lat) {
       throw new HttpException('lat is required', 400);
@@ -32,32 +32,55 @@ export class AppController {
     // class OpenWeatherService implements WeatherService {
     // class AnotherOpenWeatherService implements WeatherService {
     // TODO: implememnt WeatherService interface
-    const response = await axios.request<{
-      main: { temp: number; pressure: number; humidity: number };
-      sys: { country: string };
-      name: string;
-    }>({
-      method: 'GET',
-      url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-      params: {
-        lat: lat,
-        lon: lon,
-        units: 'metric',
-      },
-      headers: {
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        'x-rapidapi-key': process.env.RAPID_API_KEY,
-      },
-    });
+    if (alternateSource === 'false') {
+      const response = await axios.request<{
+        main: { temp: number; pressure: number; humidity: number };
+        name: string;
+      }>({
+        method: 'GET',
+        url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+        params: {
+          lat: lat,
+          lon: lon,
+          units: 'metric',
+        },
+        headers: {
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+          'x-rapidapi-key': process.env.RAPID_API_KEY,
+        },
+      });
+      const returnedObject = {
+        temperature: response.data.main.temp,
+        pressure: response.data.main.pressure,
+        humidity: response.data.main.humidity,
+        source: 'Open Weather Map',
+      };
+      return returnedObject;
+    }
+    if (alternateSource === 'true') {
+      const response = await axios.request<{
+        currently: { temperature: number; pressure: number; humidity: number };
+        name: string;
+      }>({
+        method: 'GET',
+        url: `https://dark-sky.p.rapidapi.com/${lat},${lon}`,
+        params: {
+          lang: 'en',
+          units: 'si',
+        },
+        headers: {
+          'x-rapidapi-host': 'dark-sky.p.rapidapi.com',
+          'x-rapidapi-key': process.env.RAPID_API_KEY,
+        },
+      });
 
-    const returnedObject = {
-      temperature: response.data.main.temp,
-      pressure: response.data.main.pressure,
-      humidity: response.data.main.humidity,
-      country: response.data.sys.country,
-      name: response.data.name,
-    };
-
-    return returnedObject;
+      const returnedObject = {
+        temperature: response.data.currently.temperature,
+        pressure: response.data.currently.pressure,
+        humidity: response.data.currently.humidity,
+        source: 'Dark Sky',
+      };
+      return returnedObject;
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 
-
-
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
